86a30e244b11281cc7722e4bedb55e7dbbd91c3d
shibamirai <66295034+shibamirai@users.noreply.github.com>
2023-12-08 09:28:55 +0900

    PostgreSQL対応

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.demo;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+	@Autowired
+	HelloRepository repository;
 
 	@GetMapping("/")
 	public String getHello() {
-		return "hello";
+		
+		Map<String, Object> map = repository.findById(1);
+		return (String)map.get("name");
 	}
 
 }
